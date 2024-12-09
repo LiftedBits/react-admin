62c55822724843a48c8c87ed5ff4f90b43cce906
Darshan <105104835+DarshanIITB@users.noreply.github.com>
2024-12-09 21:09:56 +0530

    basic settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css"
-import { Button, Container, Typography } from "@mui/material"
+import { Container } from "@mui/material"
+import DataTable from "./components/table"
 
 function App() {
+  // const res = use(promise)
   return (
     <Container style={{ textAlign: "center", marginTop: "50px" }}>
-      <Typography variant="h3" gutterBottom>
-        Welcome to Material-UI
-      </Typography>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
+      <DataTable />
     </Container>
   )
 }
