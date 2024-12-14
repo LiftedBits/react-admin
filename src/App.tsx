cb43feceb31e97a1dc13865bd4d4d7ad42fcb6a1
yashikant <57305635+yashikant@users.noreply.github.com>
2024-12-14 10:48:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./pages/login"
 import { PrivateRoutes } from "./components/protected-route"
 import Home from "./pages/home"
-
+import Navbar from "./components/Navbar/Navbar"
+import BreadcrumbsComponent from "./components/BreadcrumbsComponent/BreadcrumbsComponent"
 function App() {
   return (
     // <Container style={{ height: "100vh", width: "100vw" }}>
@@ -12,6 +13,10 @@ function App() {
     //   <CollectionPage collection={collections.blogs} />
     // </Container>
     <BrowserRouter>
+      <Navbar />
+      <div style={{ marginTop: "-90px" }}>
+        <BreadcrumbsComponent />
+      </div>
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route path="/" element={<Home />} />
